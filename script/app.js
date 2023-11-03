f4049cd576d7307320a42831a165a2d3ef600d98
jeongsoyul <141130032+jeongsoyul@users.noreply.github.com>
2023-11-03 16:56:32 +0900

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,112 @@
+
+// 햄버거바 애니메이션
+   $(document).ready(function () {
+    $('#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+    });
+
+    // 네비메뉴 마우스 오버
+    $(".openmenu ul li a").mouseover(function () {
+        $(".circle--2").stop(true).animate({ width: '30px', height: '30px' })
+    });
+    $(".openmenu ul li a").mouseout(function () {
+        $(".circle--2").stop(true).animate({ width: '6px', height: '6px' })
+    });
+
+
+// open페이지 열고 닫기
+    let banner = $(".openbox");
+    let btn = $("#nav-icon4");
+    
+    btn.click(function(){
+        banner.toggleClass("open");
+
+        if(banner.hasClass("open")){
+            banner.fadeIn()
+            btn.find("span").css({backgroundColor:'#fff'});
+            $(".h_inner h1 img").attr('src','./img/flogo.svg')
+            $(".h_inner h1 img").css({width:'200px'})
+        }else{
+            banner.fadeOut()
+            btn.find("span").css({backgroundColor:'#000'});
+            $(".h_inner h1 img").attr('src','./img/logo.svg')
+        }
+    });      
+      
+
+    // 비디오 버튼 눌렀을때 버튼사라지게
+    $(".v_inner .v_btn").click(function () {
+        $(".v_inner .v_btn ").hide()
+    });
+
+    // 자동재생  // 익스플로어 재생 안됌 수정※
+    var video = document.querySelector("video"),
+        button = document.querySelector(".v_btn");
+
+
+    button.addEventListener("click", function () {
+        video.play()
+        //video.setAttribute("autoplay","autoplay")// this will set the attribute but will not play 
+    }, false);
+   
+
+
+
+    
+    // 메인 슬라이드
+    
+
+    //con슬라이드
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
+  //scoll top
+
+  let Tbtn = $(".btnTop"); //Tbtn = top button의미
+
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 100) {
+      Tbtn.stop(true).fadeIn();
+    } else {
+      Tbtn.stop(true).fadeOut();
+    }
+  }); //스크롤을 100을 내렸을때 버튼이 사라졌다가 원래대로하면 보이게
+
+  Tbtn.click(() => {
+    $(window).scrollTop(0);
+    //수직상의 (위치값) 으로 이동
+    //버튼을 클릭했을 때 창이 0값으로 올라감
+  });
+
+
+
+// 스크롤시 musium 글보이게
+  $(window).scroll( function(){
+    $('.musium .mbox .Bbox span').each( function(i){
+        
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        if( bottom_of_window > bottom_of_element ){
+            $(this).animate({'opacity':'1','margin-left':'0px'},500);
+        }
+        
+    }); 
+});
+
+
+    /////////////////////////////
+});
+
